53231808c4a739e6b8127f2c278be248daf7f31d
Rafael <rafael@skysoul.com.au>
2016-02-14 15:51:42 +0000

    enter page test

--- Application/Common/Common/typescript/App.ts
+++ Application/Common/Common/typescript/App.ts
@@ -1,10 +1,20 @@
-
 'use strict';
 
 module Lanhu {
 
     export var debug:boolean = true;
 
+    export function Start() {
+
+
+        var bg = document.querySelector('.intro__background__left .--bg');
+
+        window.addEventListener('click', function () {
+            //bg.style.opacity = 0
+        }, false);
+
+    }
+
 }
 
-//var app = new Lanhu.Gulp();
+var app = new Lanhu.Start();
--- Public/js/app.js
+++ Public/js/app.js
@@ -2,6 +2,13 @@
 var Lanhu;
 (function (Lanhu) {
     Lanhu.debug = true;
+    function Start() {
+        var bg = document.querySelector('.intro__background__left .--bg');
+        window.addEventListener('click', function () {
+            //bg.style.opacity = 0
+        }, false);
+    }
+    Lanhu.Start = Start;
 })(Lanhu || (Lanhu = {}));
-//var app = new Lanhu.Gulp(); 
+var app = new Lanhu.Start();
 //# sourceMappingURL=app.js.map
